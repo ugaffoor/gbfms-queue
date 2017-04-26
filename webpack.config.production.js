51eabfb7153d7fcc0c7aa3087f561af2058fb663
Matt Raykowski <matt.raykowski@kineticdata.com>
2017-04-26 13:17:20 -0500

    Fixed production build.

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 module.exports = {
   entry: './src/prodEntry.js',
@@ -15,15 +16,35 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         use: [
-          'babel-loader'
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
         ],
-        exclude: /node_modules/
+        exclude: path.resolve(__dirname, 'node_modules'),
       },
+      {
+        test: /\.(jade|pug)$/,
+        use: [
+          {
+            loader: 'ngtemplate-loader',
+            options: {
+              module: 'kd.bundle.angular.templates',
+              relativeTo: path.resolve(__dirname, './app'),
+              requireAngular: true
+            }
+          },
+          { loader: 'pug-html-loader' }
+        ]
+      },
+      { test: require.resolve('jquery'), loader: 'expose-loader?jQuery!expose-loader?$' },
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
       },
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
@@ -49,6 +70,18 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       comments: false
+    }),
+    // do not emit compiled assets that include errors
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      toastr: 'toastr',
+      moment: 'moment'
+    }),
+
+    // Apply ng-annotate to Angular components, etc.
+    new NgAnnotatePlugin({
+      add: true
     })
   ]
 };
